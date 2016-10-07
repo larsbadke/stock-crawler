542a0d681935d12598e7c15fdbe38ad0ef3adf34
Lars Badke <l.badke@gmx.net>
2016-10-07 11:21:16 +0200

    addes conditionstest

---
+++ test/Crawler/ConditionsTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace StockCrawler\Test;
+
+
+use StockCrawler\Conditions;
+
+class ConditionsTest extends \PHPUnit_Framework_TestCase
+{
+    
+    
+    public function test_multiple_conditions()
+    {
+        $example = "price() > 100 and RSI(14) >= 30;\n\r open() >= 99;";
+        
+        $conditions = new Conditions($example);
+        
+        $this->assertCount(2, $conditions->get());
+
+        $this->assertEquals(2, $conditions->count());
+    }
+
+  
+
+}
